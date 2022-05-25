c4f88354fac288b98c7cb1edf0577e74e66b8395
lubieplacki <m.goralczyk@vasco-electronics.com>
2022-05-25 07:33:57 +0200

    usunięta prośba o usera i haslo, magento i tak zapyta.

--- install/download.sh
+++ install/download.sh
@@ -7,7 +7,3 @@ VERSION=${3:-2.4.4}
 EDITION=${4:-community}
 
 exec composer create-project --repository-url=https://repo.magento.com/ magento/project-"${EDITION}"-edition="${VERSION}" ../src/
-echo
-echo "Composer authentication required (repo.magento.com public and private keys):"
-read -r -p "    Username: " ${USER}
-read -r -p "    Password: " ${PASS}
